3d96890fdcb2f292c853195303c3c18d9056e5ad
Jacob Dearing <jacob.dearing@gmail.com>
2011-10-31 14:06:43 -0500

    initial commit

---
+++ src/com/dearing/remote/StartN64.java
@@ -0,0 +1,13 @@
+package com.dearing.remote;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class StartN64 extends ListActivity {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.roms_n64);
+
+	}
+}
